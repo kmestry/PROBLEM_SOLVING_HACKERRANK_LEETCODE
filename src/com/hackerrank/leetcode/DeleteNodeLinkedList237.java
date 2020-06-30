83d6b3e00dd6d02b326cbcb01035a800d642bbed
Kunal Mestry <61279086+kmestry@users.noreply.github.com>
2020-07-01 01:55:38 +0530

    leet code solutions with problem number in class name.

---
+++ src/com/hackerrank/leetcode/DeleteNodeLinkedList237.java
@@ -0,0 +1,21 @@
+package com.hackerrank.leetcode;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class DeleteNodeLinkedList237 {
+
+    public void deleteNode(ListNode node) {
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+    }
+}
---
+++ src/com/hackerrank/leetcode/ReverseStringJuneChallenge344.java
@@ -0,0 +1,15 @@
+package com.hackerrank.leetcode;
+
+public class ReverseStringJuneChallenge344 {
+
+    public void reverseString(char[] s) {
+
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = temp;
+        }
+
+
+    }
+}
