181613cef40cb410c68ad3c0c4b0aa604e8b315c
KunKing <kunking951@gmail.com>
2020-07-02 02:07:01 +0530

    leeet code solution .

---
+++ src/com/hackerrank/leetcode/CountNumber1351.java
@@ -0,0 +1,45 @@
+package com.hackerrank.leetcode;
+
+public class CountNumber1351 {
+
+    public static void main(String[] args) {
+        //[[3,2],[-3,-3],[-3,-3],[-3,-3]]
+        //int result = countNegatives(new int[][]{{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}});
+        int result = countNegatives(new int[][]{{3, 2}, {-3, -3}, {-3, -3}, {-3, -3}});
+        System.out.println("result = " + result);
+    }
+
+    public static int countNegatives(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int counter = 0;
+
+        for (int i = 0; i < row; i++) {
+            int[] innerArray = new int[col];
+            for (int j = 0; j < col; j++) {
+                innerArray[j] = grid[i][j];
+            }
+            //edge case
+            if (innerArray[0] < 0) {
+                counter += innerArray.length;
+            } else if (innerArray[innerArray.length - 1] > 0) {
+                counter += 0;
+            } else {
+                int left = 0;
+                int right = innerArray.length - 1;
+                while (left <= right) {
+                    int mid = left + ((right - left) / 2);
+                    if (innerArray[mid] >= 0) {
+                        left = mid + 1;
+                    } else if (innerArray[mid] < 0) {
+                        right = mid - 1;
+                    }
+                }
+                counter += innerArray.length - left;
+            }
+        }
+        return counter;
+    }
+}
+
+
---
+++ src/com/hackerrank/leetcode/RandomPickWeight.java
@@ -0,0 +1,49 @@
+package com.hackerrank.leetcode;
+
+import java.util.Arrays;
+
+public class RandomPickWeight {
+    static int[] auxArray;
+    static int sumCounter = 0;
+    int[] w;
+    int sumOfArray = 0;
+    int counter = 0;
+
+    public RandomPickWeight(int[] w) {
+        this.w = w;
+        auxArray = new int[w.length];
+        this.sumOfArray = Arrays.stream(w).sum();
+        Arrays.sort(w);
+
+    }
+
+    public static void main(String[] args) {
+        RandomPickWeight randomPickWeight = new RandomPickWeight(new int[]{1, 3});
+        System.out.println(randomPickWeight.pickIndex());
+        System.out.println(randomPickWeight.pickIndex());
+        System.out.println(randomPickWeight.pickIndex());
+        System.out.println(randomPickWeight.pickIndex());
+        System.out.println(randomPickWeight.pickIndex());
+        System.out.println(randomPickWeight.pickIndex());
+    }
+
+    public int pickIndex() {
+
+        for (int i = 0; i < w.length; i++) {
+            while (w[i] != auxArray[i]) {
+                auxArray[i] = counter + 1;
+                counter++;
+                if (counter == w[i]) {
+                    counter = 0;
+                }
+                sumCounter++;
+                if (sumCounter == sumOfArray) {
+                    auxArray = new int[w.length];
+                    sumCounter = 0;
+                }
+                return i;
+            }
+        }
+        return 0;
+    }
+}
