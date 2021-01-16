452223d1e0f85be5d5fb9ab7921515698d186819
kmestry <61279086+kmestry@users.noreply.github.com>
2021-01-17 02:14:43 +0530

    print sequence using three threads

---
+++ src/com/java/thread/printoddeven/RunnableTask.java
@@ -0,0 +1,51 @@
+package com.java.thread.printoddeven;
+
+
+public class RunnableTask implements Runnable {
+    static Object lock = new Object();
+    static int number = 1;
+    int remainder;
+    public RunnableTask(int remainder) {
+        this.remainder = remainder;
+    }
+
+    public static void main(String[] args) {
+        RunnableTask p1 = new RunnableTask(0);
+        RunnableTask p2 = new RunnableTask(1);
+        RunnableTask p3 = new RunnableTask(2);
+
+        Thread t1 = new Thread(p1, "thread p1");
+        Thread t2 = new Thread(p2, "thread p2");
+        Thread t3 = new Thread(p3, "thread p3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+        //
+    }
+
+    @Override
+    public void run() {
+
+
+        while (number < 9) {
+            synchronized (lock) {
+                //System.out.println("===");
+
+                while (number % 3 != remainder) {
+
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                System.out.println("number is =" + number);
+                number++;
+                lock.notifyAll();
+            }
+        }
+    }
+}
