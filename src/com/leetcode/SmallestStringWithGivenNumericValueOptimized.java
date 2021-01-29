6a7a862d86248244aff0c6965b2fbe470e153124
kmestry <61279086+kmestry@users.noreply.github.com>
2021-01-29 14:18:58 +0530

    leetcode jan challenge

---
+++ src/com/leetcode/SmallestStringWithGivenNumericValueOptimized.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+public class SmallestStringWithGivenNumericValueOptimized {
+
+    public static void main(String[] args) {
+
+        new SmallestStringWithGivenNumericValueOptimized().getSmallestString(5, 73);
+    }
+
+    public String getSmallestString(int n, int k) {
+        char ans[] = new char[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = 'a';
+            k--;
+        }
+
+        int index = ans.length - 1;
+        while (k >= 26) {
+            ans[index] = 'z';
+            k = k + 1 - 26;
+            index--;
+        }
+
+        ans[index] = (char) (k + 'a');
+
+        return String.valueOf(ans);
+    }
+}
