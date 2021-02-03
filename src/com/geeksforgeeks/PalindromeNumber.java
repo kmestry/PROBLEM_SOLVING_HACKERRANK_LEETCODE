56d80d8aa21c3d3235d93d67cc3712cdd600637b
kmestry <61279086+kmestry@users.noreply.github.com>
2021-02-04 03:44:06 +0530

    geeks for geeks course

--- src/com/geeksforgeeks/PalindromeNumber.java
+++ src/com/geeksforgeeks/PalindromeNumber.java
@@ -3,10 +3,24 @@ package com.geeksforgeeks;
 public class PalindromeNumber {
 
     public static void main(String[] args) {
-        boolean res = new PalindromeNumber().isPalindrome(431134);
+        boolean res = new PalindromeNumber().isPalindromeOptimized(Integer.MAX_VALUE);
         System.out.println("res = " + res);
     }
 
+    private boolean isPalindromeOptimized(int number) {
+
+        int rev = 0;
+        int origNum = number;
+        while (number != 0) {
+
+            rev = rev * 10 + number % 10;
+            number /= 10;
+        }
+
+        System.out.println("rev = " + rev);
+        return rev == origNum;
+
+    }
 
     private boolean isPalindrome(int number) {
 
@@ -14,16 +28,11 @@ public class PalindromeNumber {
         int power = (int) Math.floor(Math.log10(number) + 1) - 1;
         int reverseNum = 0;
         while (number > 0) {
-
             int remainder = number % 10;
             number /= 10;
-
             reverseNum += Math.pow(10, power) * remainder;
-
             power--;
-
         }
-
         return origNumber == reverseNum;
     }
 }
