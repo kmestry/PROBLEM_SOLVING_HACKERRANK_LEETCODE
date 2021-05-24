d371ec3a4ddb34303d99d5d2b5b0c8b53a7f853e
kmestry <61279086+kmestry@users.noreply.github.com>
2021-05-25 04:59:14 +0530

    geeks for geeks string problems

---
+++ src/com/geeksforgeeks/strings/IsAnagram.java
@@ -0,0 +1,28 @@
+package com.geeksforgeeks.strings;
+
+public class IsAnagram {
+
+    //Function is to check whether two strings are anagram of each other or not.
+    public static boolean isAnagram(String a, String b) {
+
+        // Your code here
+
+        char[] array = new char[128];
+
+        for (int i = 0; i < a.length(); i++) {
+            array[a.charAt(i)]++;
+        }
+
+        for (int i = 0; i < b.length(); i++) {
+            array[b.charAt(i)]--;
+        }
+
+        for (char c : array) {
+            if ((int) c > 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
