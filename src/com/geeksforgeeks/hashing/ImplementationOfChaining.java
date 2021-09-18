6eed0dd5e8983450a978e86e6ce88990522c0b05
kmestry <61279086+kmestry@users.noreply.github.com>
2021-09-19 04:15:40 +0530

    Hashing geeks for geeks course.

---
+++ src/com/geeksforgeeks/hashing/ImplementationOfChaining.java
@@ -0,0 +1,52 @@
+package com.geeksforgeeks.hashing;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ImplementationOfChaining {
+
+    int bucketSize;
+    List<LinkedList<Integer>> list;
+
+    ImplementationOfChaining(int bucketSize) {
+        this.bucketSize = bucketSize;
+        list = new ArrayList<>();
+
+        for (int i = 0; i < bucketSize; i++) {
+            list.add(new LinkedList<>());
+        }
+    }
+
+    void insert(int value) {
+        int index = value % bucketSize;
+        LinkedList<Integer> linkedList = list.get(index);
+        linkedList.add(value);
+    }
+
+    void delete(int value) {
+        int index = value % bucketSize;
+        LinkedList<Integer> linkedList = list.get(index);
+        linkedList.remove((Integer) value);
+    }
+
+    boolean search(int value) {
+        int index = value % bucketSize;
+        LinkedList<Integer> linkedList = list.get(index);
+        return linkedList.contains(value);
+    }
+
+    public static void main(String[] args) {
+        ImplementationOfChaining implementationOfChaining = new ImplementationOfChaining(5);
+        implementationOfChaining.insert(10);
+        implementationOfChaining.insert(22);
+        implementationOfChaining.insert(30);
+
+        implementationOfChaining.delete(30);
+
+        System.out.println(implementationOfChaining.search(22));
+    }
+
+}
+
+
